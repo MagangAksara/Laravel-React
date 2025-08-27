215835a8ba2151030a379de933a8d9ee245c8e2e
Dimas <124556325+Diz09@users.noreply.github.com>
2025-08-27 14:54:11 +0700

    fix view

--- resources/js/Pages/Owner/Konten/DashboardComponent/TopCard.jsx
+++ resources/js/Pages/Owner/Konten/DashboardComponent/TopCard.jsx
@@ -6,7 +6,7 @@ const TopCard = ({ name, date, time }) => {
 
     return (
         <>
-            <div className="relative bg-gradient-to-r from-blue-700 to-blue-300 lg:to-sky-100 text-white rounded-2xl p-6 mt-3 mb-8 flex flex-col lg:flex-row justify-between items-start lg:items-center shadow-md w-full lg:w-[95%]">
+            <div className="relative bg-gradient-to-r from-blue-700 to-blue-300 md:to-sky-100 text-white rounded-2xl p-6 mt-3 mb-8 flex flex-col md:flex-row justify-between items-start md:items-center shadow-md w-full md:w-[85%] lg:w-[92%]">
                 <div className="z-10">
                     <div className="flex items-center gap-2 bg-white/20 px-3 py-1 rounded-lg text-sm mb-4 w-fit">
                         <Calendar className="w-4 h-4" />
@@ -17,11 +17,11 @@ const TopCard = ({ name, date, time }) => {
                     </h2>
                     <p className="text-sm opacity-90">Have a nice day!</p>
                 </div>
-                <div className="hidden md:flex mt-4 lg:mt-0 lg:absolute lg:-right-[90px] lg:-bottom-[52px]">
+                <div className="hidden md:flex mt-4 md:mt-0 md:absolute md:-right-[100px] md:-bottom-[32px]">
                     <img
                         src="/pictures/dashboard/owner/car-illustration.png"
                         alt="Car Rent Illustration"
-                        className="w-60 lg:w-80"
+                        className="w-40 md:w-[250px]"
                     />
                 </div>
             </div>
