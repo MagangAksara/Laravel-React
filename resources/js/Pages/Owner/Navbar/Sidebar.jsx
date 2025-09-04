3d7728e3908ad2a0509f30463e8da1db404345bd
Dimas <124556325+Diz09@users.noreply.github.com>
2025-09-04 10:11:30 +0700

    fix sidebar

--- resources/js/Pages/Owner/Navbar/Sidebar.jsx
+++ resources/js/Pages/Owner/Navbar/Sidebar.jsx
@@ -6,30 +6,32 @@ const Sidebar = () => {
   const { url } = usePage();
 
   const menu = [
-    { name: "Dashboard", icon: <Home size={20} />, link: route("owner.dashboard")},
-    { name: "Orders Management", icon: <FileText size={20} />, link: route("owner.orders.management") },
-    { name: "Cars Management", icon: <Car size={20} />, link: route("owner.cars.management") },
+    { name: "Dashboard", icon: <Home size={20} />, routeName: "owner.dashboard" },
+    { name: "Orders Management", icon: <FileText size={20} />, routeName: "owner.orders.management" },
+    { name: "Cars Management", icon: <Car size={20} />, routeName: "owner.cars.management" },
   ];
 
   return (
     <aside className="bg-blue-600 text-white min-h-screen p-4 rounded-tr-[60px] w-20 md:w-60 transition-all duration-300 ease-in-out group">
       <nav className="flex flex-col gap-2 mt-5">
-        {menu.map((item, idx) => (
-          <Link
-            key={idx}
-            href={item.link}
-            className={`flex items-center gap-3 px-3 py-3 rounded-lg transition
-              ${url.startsWith(item.link)
-                ? "bg-white text-blue-800 font-semibold"
-                : "hover:bg-blue-500"
-              }`}
-          >
-            <span>{item.icon}</span>
-            <span className="hidden md:inline">
-              {item.name}
-            </span>
-          </Link>
-        ))}
+        {menu.map((item, idx) => {
+          const isActive = route().current(item.routeName);
+
+          return (
+            <Link
+              key={idx}
+              href={route(item.routeName)}
+              className={`flex items-center gap-3 px-3 py-3 rounded-lg transition
+                ${isActive
+                  ? "bg-white text-blue-800 font-semibold"
+                  : "hover:bg-blue-500"
+                }`}
+            >
+              <span>{item.icon}</span>
+              <span className="hidden md:inline">{item.name}</span>
+            </Link>
+          );
+        })}
       </nav>
     </aside>
   );
