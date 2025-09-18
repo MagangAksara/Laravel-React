bcddd8a552786718d6b2c667737a30cb60619327
Dimas <124556325+Diz09@users.noreply.github.com>
2025-09-19 02:12:21 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -68,12 +68,11 @@ Route::middleware(['auth', 'role:owner'])->group(function () {
 
     Route::get('/cars', [CarManagementController::class, 'index'])->name('owner.cars.management');
     Route::get('/cars/{id}', [CarManagementController::class, 'show'])->name('owner.cars.detail');
-    Route::post('/cars/{id}', [CarManagementController::class, 'update'])->name('owner.cars.update');
-    Route::delete('/cars/{id}', [CarManagementController::class, 'destroy'])->name('owner.cars.destroy');
-
-
+    Route::post('/cars/store-step-basic', [CarManagementController::class, 'storeStepBasic'])->name('owner.cars.storeStepBasic');
     Route::post('/brands', [CarManagementController::class, 'store'])->name('owner.brands.store');
     Route::post('/models', [CarManagementController::class, 'modelsStore'])->name('owner.models.store');
+    Route::post('/cars/{id}', [CarManagementController::class, 'update'])->name('owner.cars.update');
+    Route::delete('/cars/{id}', [CarManagementController::class, 'destroy'])->name('owner.cars.destroy');
 });
 
 Route::middleware('auth')->group(function () {
