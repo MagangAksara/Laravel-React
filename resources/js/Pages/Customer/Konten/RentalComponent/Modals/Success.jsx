838fe35908529304234c9b29862cd5b32c200443
Dimas <124556325+Diz09@users.noreply.github.com>
2025-09-26 15:49:07 +0700

    fix composer vite

--- resources/js/Pages/Customer/Konten/RentalComponent/Modals/Success.jsx
+++ resources/js/Pages/Customer/Konten/RentalComponent/Modals/Success.jsx
@@ -1,19 +1,16 @@
 import { Head } from "@inertiajs/react";
-import Navbar from "@/Pages/ComponetGlobal/Navbar";
-import PageHeader from "@/Pages/ComponetGlobal/PageHeader";
+import Layout from "../../../Layout";
 
 export default function Success() {
   return (
     <>
-      <Head title="Success"/>
-      <Navbar
-        header={<PageHeader/>}
-      >
+      <Head title="Success" />
+      <Layout>
         <div className="p-10 text-center">
           <h1 className="text-2xl font-bold text-green-600">Pembayaran Berhasil 🎉</h1>
           <p>Terima kasih sudah melakukan pembayaran.</p>
         </div>
-      </Navbar>
+      </Layout>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,17 @@ export default defineConfig({
         react(),
         svgr(),
     ],
+    // server: {
+    //     host: '0.0.0.0',
+    //     port: 5173,
+    //     origin: 'https://040495e9f4ad.ngrok-free.app:5173', // domain ngrok kamu
+    //     hmr: {
+    //         host: '040495e9f4ad.ngrok-free.app',
+    //         protocol: 'wss',
+    //         port: 5173,
+    //         clientPort: 443,
+    //     },
+    // },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'resources/js'),
